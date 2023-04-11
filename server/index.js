639168cf779703f976ccf9d6cfac380a2c45a11a
emdadulemj <emadadulrahman2212@gmail.com>
2023-04-11 15:19:22 +0600

    class-61

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const dbConnection = require("./config/dbConfig.js")
 const routes = require("./routes")
+var cors = require('cors')
 
 
 // Database Connection
 dbConnection()
 
 // middleware
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
